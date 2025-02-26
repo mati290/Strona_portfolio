e113e073a3c07defbf6bae9268534c26d18617d5
mati290e <mati.poreba@wp.pl>
2025-02-26 18:22:31 +0100

    Initial commit

---
+++ projects/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    link = models.URLField()
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
